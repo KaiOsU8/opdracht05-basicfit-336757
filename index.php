60625fee0ca128f0c729c2dbff728eae03547ea3
KaiOsU8 <kokokovachev10@gmail.com>
2023-03-06 16:33:59 +0100

    basicfit 4

--- index.php
+++ index.php
@@ -9,38 +9,47 @@
     <title>PDO CRUD</title>
 </head>
 <body>
-    <h1>PDO CRUD</h1>
+    <h1>Basic-Fit Utrecht</h1>
 
     <form action="create.php" method="post">
         <label for="homeclub">Kies je homeclub:</label><br>
         <select name="homeclub" id="homeclub">
             <option value="">Kies je locatie</option>
-            <option value="">Franciscusdreef 80</option>
-            <option value=""></option>
-            <option value=""></option>
-            <option value=""></option>
-            <option value=""></option>
-            <option value=""></option>
-            <option value=""></option>
+            <option value="Franciscusdreef 80">Franciscusdreef 80</option>
+            <option value="Middenweteringen 21">Middenweteringen 21</option>
+            <option value="Zonnebaan 22">Zonnebaan 22</option>
+            <option value="Moreelsehoek 2">Moreelsehoek 2</option>
+            <option value="Van Heuven Goedhartplein 13">Van Heuven Goedhartplein 13</option>
+            <option value="Europaplein 705">Europaplein 705</option>
+            <option value="Herculesplein 375-77">Herculesplein 375-77</option>
         </select><br><br>
 
-        <p>Selecteer een lidmaatschap</p><br>
-        <label for="">Lidmaatschap</label><br>
-        <input type="text" name="" id=""><br><br>
 
-        <p>Looptijd:</p><br>
-        <label for="">Looptijd</label><br>
-        <input type="text" name="" id=""><br><br>
+        <h3>Selecteer een lidmaatschap</h3>
+        <label for="comfort">Comfort</label><br>
+        <input type="radio" name="lidmaatschap" id="comfort" value="comfort"><br>
+        <label for="premium">Premium</label><br>
+        <input type="radio" name="lidmaatschap" id="premium" value="premium"><br>
+        <label for="all">All in</label><br>
+        <input type="radio" name="lidmaatschap" id="all" value="all"><br><br>
 
-        <p>Selecteer je extra's</p><br>
-        <label for="">Yanga</label><br>
-        <input type="text" name="" id=""><br><br>
 
-        <label for="">Coach</label><br>
-        <input type="text" name="" id=""><br><br>
+        <h3>Looptijd:</h3>
+        <label for="jaarlidmaatschap">Jaarlidmaatschap</label><br>
+        <input type="radio" name="looptijd" id="jaarlidmaatschap" value="jaarlidmaatschap"><br>
+        <label for="flex">Flex optie</label><br>
+        <input type="radio" name="looptijd" id="flex" value="flex"><br>
 
-        <label for="">Training</label><br>
-        <input type="text" name="" id=""><br><br>
+
+        <h3>Selecteer je extra's</h3>
+        <label for="yanga">Yanga</label><br>
+        <input type="checkbox" name="yanga" id="yanga"><br>
+
+        <label for="coach">Coach</label><br>
+        <input type="checkbox" name="coach" id="coach"><br>
+
+        <label for="training">Training</label><br>
+        <input type="checkbox" name="training" id="training"><br><br><br>
 
         <label for="email">E-mail</label><br>
         <input type="text" name="email" id="email"><br><br>
